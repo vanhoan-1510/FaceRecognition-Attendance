a7d132fbc7a06400976b9dbd0fe53e99f5203945
KhanhKid2908 <119712866+KhanhKid2908@users.noreply.github.com>
2023-02-21 11:53:45 +0700

    waitkey(q)

--- attendance.py
+++ attendance.py
@@ -4,8 +4,6 @@ import face_recognition
 import os
 from datetime import datetime
 
-# from PIL import ImageGrab
-
 path = 'ImagesAttendance'
 images = []
 classNames = [] 
@@ -24,15 +22,16 @@ def findEncodings(images):
 
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
+        faces = face_recognition.face_locations(img)
+        if len(faces) > 0:
+            encode = face_recognition.face_encodings(img, faces)[0]
+            encodeList.append(encode)
     return encodeList
 
 
 def markAttendance(name):
     with open('Attendance.csv', 'r+') as f:
         myDataList = f.readlines()
-
 
         nameList = []
         for line in myDataList:
@@ -43,12 +42,6 @@ def markAttendance(name):
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
 
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
-
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
@@ -56,7 +49,6 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    # img = captureScreen()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
@@ -66,12 +58,10 @@ while True:
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        # print(faceDis)
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            # print(name)
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
@@ -80,4 +70,5 @@ while True:
             markAttendance(name)
 
     cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+    if cv2.waitKey(50) & 0xFF == ord('q'):
+        break
